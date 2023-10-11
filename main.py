609e2ace891a2ecac566974c47c5d7fe7920f7d2
jiach <jiachengfriend@gmail.com>
2023-10-11 01:14:14 -0600

    Started planning the program

---
+++ main.py
@@ -0,0 +1,15 @@
+# main.py
+"""
+Title: Main Web Scraper
+Author: Jason
+Date Created: 2023-10-11
+"""
+# This program gets dictionary data on the internet and converts to a csv.
+
+# Create a csv file.
+
+# Web scrape.
+
+# Run
+if __name__ == "__main__":
+    pass
