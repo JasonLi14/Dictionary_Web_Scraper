8a6cdf67737fe768b64c8a0583ab742d99a94da1
jiach <jiachengfriend@gmail.com>
2023-10-13 01:01:44 -0600

    Got definitions and etymologies. Working on pronunciations

--- main.py
+++ main.py
@@ -12,33 +12,47 @@ import re
 from requests import get
 
 regex = r'[^\x00-\x7F]+'
+headers = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:108.0) Gecko/20100101 Firefox/108.0"}
 
 class Dictionary:
     def remove_non_ascii(self, text):
         return re.sub(regex, '', text)
 
     def get_soup(self, url):
-        raw = self.remove_non_ascii(urlopen(url).read().decode('utf-8'))
-        soup = bs(raw)
-        print(soup.select("#MainTxt")[0].select('.ds-single')[1].text.strip())
-        return soup.select("#MainTxt")[0].select('.ds-single')[0].text.strip()
+        response = get(url=url, headers=headers).text
+        response = self.remove_non_ascii(response)
+        soup = bs(response, "html.parser").select(".main-container")[0]
+        print(soup)
+        definitions = []
+        for i in range(len(soup.select('.dtText'))):  # add the definitions
+            # dtText is the class they belong to
+            definition = soup.select('.dtText')[i].text.strip()
+            # print(definition)
+            if len(definition) > 1:  # clean up definition
+                if definition[len(definition)-1] == ":":
+                    definition = definition[:len(definition)-1]
+                if definition[0:2] == ": ":
+                    definition = definition[2:]
+
+                definitions.append(definition)
+
+        # get part of speech
+        part_of_speech_list = []
+        for i in range(len(soup.select('.parts-of-speech'))):  # add the definitions
+            part_of_speech = soup.select('.parts-of-speech')[i].text.strip()
+            part_of_speech_list.append(part_of_speech)
+
+        # other
+        etymology = soup.select('.et')[0].text.strip()
+        syllables = soup.select('.word-syllables-entry')[0].text.strip()
+        return definitions, part_of_speech_list, etymology
+
 
     def lookup(self, word):
-        base_url = "http://www.thefreedictionary.com/"
+        # base_url = "http://www.thefreedictionary.com/"
+        base_url = "https://www.merriam-webster.com/dictionary/"
         query_url = base_url + word
         return self.get_soup(query_url)
-
-"""
-url = "http://olympus.realpython.org/profiles/aphrodite"
-page = urlopen(url)
-html_bytes = page.read()
-html = html_bytes.decode("utf-8")
-title_index = html.find("<title>")
-start_index = title_index + len("<title>")
-end_index = html.find("</title>")
-title = html[start_index:end_index]
-soup = BeautifulSoup(html, "html.parser")
-"""
 
 
 # Data structures
@@ -69,15 +83,6 @@ def web_scrape(word):
     :param word: string
     :return: wordObject
     """
-    """new_word_obj = Word(word)
-    print(new_word_obj.link)
-    page = urlopen(new_word_obj.link)
-    # use the beautiful soup the parse
-    html = page.read().decode("utf-8")
-    soup = bs(html, "html.parser")
-    # print(soup)
-    definitions = soup.find_all("beautiful")
-    print(definitions)"""
     the_dict = Dictionary()
     definition = the_dict.lookup(word)
     print(definition)
